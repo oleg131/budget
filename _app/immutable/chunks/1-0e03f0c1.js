bedc79f493f8ccbb7f2539b2ada27cedec8b1c55
oleg131 <dkfjvn@gmail.com>
2023-08-05 16:50:23 -0700

    Updates

---
+++ _app/immutable/chunks/1-0e03f0c1.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/error.svelte-6cd67c2e.js";export{t as component};
--- _app/immutable/chunks/2-e83aa4cb.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-ac38b7cd.js";export{t as component};
